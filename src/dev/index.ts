0c749d7551e5573e69567280b34e7342e9a0f120
Geoffrey Holbrook <geof@Geoffreys-MacBook-Pro.local>
2020-04-05 19:53:47 -0400

    more dev stuff

--- src/dev/index.ts
+++ src/dev/index.ts
@@ -1,3 +1,10 @@
 export function consoleDeleteMe(...args: any[]) {
     console.log(...args)
 }
+
+export function showJSON(json: object) {
+    console.log(JSON.stringify(json, null, 4))
+}
+
+// show json delete me
+export const sjdm = showJSON
